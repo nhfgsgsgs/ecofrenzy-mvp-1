9960b9852a297be12cf8694790fa0ff694bf4fa1
tungjav <tungchuyenthainguyen@gmail.com>
2023-06-14 02:11:57 +0700

    ok

--- server/Controller/ImageController.js
+++ server/Controller/ImageController.js
@@ -1,6 +1,9 @@
 const { s3Uploadv3 } = require("../s3Service");
 const multer = require("multer");
 const User = require("../Models/User");
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "ap-southeast-1" });
 
 module.exports.upload = async (req, res, next) => {
   try {
@@ -23,7 +26,25 @@ module.exports.upload = async (req, res, next) => {
     const mission = user?.todayMission?.filter((mission) => {
       return mission.status == "Pending";
     })[0];
-
+    // call SNS aws
+    var params = {
+      Message: `${result.Location}` /* required */,
+      TopicArn:
+        "arn:aws:sns:ap-southeast-1:885537931206:ImageChallengeNotificationTopic",
+    };
+    var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
+      .publish(params)
+      .promise();
+    publishTextPromise
+      .then(function (data) {
+        console.log(
+          `Message ${params.Message} sent to the topic ${params.TopicArn}`
+        );
+        console.log("MessageID is " + data.MessageId);
+      })
+      .catch(function (err) {
+        console.error(err, err.stack);
+      });
     return res.status(200).json({
       success: true,
       message: "Image uploaded successfully",
