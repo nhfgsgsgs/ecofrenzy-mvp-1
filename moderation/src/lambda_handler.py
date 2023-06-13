8d2b9b1ae6aaa340b13e26bbfe6638ca69e7efaf
phamson02 <phamtienson02@gmail.com>
2023-06-14 03:05:52 +0700

    test image moderation func

--- moderation/src/lambda_handler.py
+++ moderation/src/lambda_handler.py
@@ -19,5 +19,37 @@ def lambda_handler(event, context):
     message = event["Records"][0]["Sns"]["Message"]
     message_data = json.loads(message)
 
-    print("Message data:")
-    print(message_data)
+    # Retrieve the S3 bucket and key from the message
+    url = message_data["url"]
+    bucket = url.split("/")[2].split(".")[0]
+    key = "/".join(url.split("/")[3:])
+
+    # Moderation with Amazon Rekognition
+    response_rekognition = rekognition_client.detect_moderation_labels(
+        Image={"S3Object": {"Bucket": bucket, "Name": key}}
+    )
+    print("Detected labels for " + key)
+    print(response_rekognition)
+
+    # VQA with Amazon SageMaker
+    request_body = {
+        "question": "Is this image safe for work?",
+        "image": url,
+    }
+    payload = json.dumps(request_body)
+
+    print("Request payload for " + key)
+    print(payload)
+
+    print("Invoking SageMaker endpoint: " + endpoint_name)
+
+    response_sagemaker = sagemaker_runtime_client.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType="application/json",
+        Body=payload,
+    )
+
+    response_payload = json.load(response_sagemaker["Body"])
+
+    print("VQA result for " + key)
+    print(response_payload)
