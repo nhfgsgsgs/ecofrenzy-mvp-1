a2c93f4d8a9fda5030adada0667a8b74b887766e
tungjav <tungchuyenthainguyen@gmail.com>
2023-06-14 22:41:38 +0700

    ok

--- moderation/src/lambda_handler.py
+++ moderation/src/lambda_handler.py
@@ -26,9 +26,9 @@ def lambda_handler(event, context):
     key = "/".join(url.split("/")[3:])
     mission = message_data["mission"]
     # should be as type list of questions
-    verif_questions = mission['verification'][0]["question"]
+    verif_questions = mission["verification"][0]["question"]
     # as type list of answers respectively
-    desired_answers = mission['verification'][0]["desiredAnswer"]
+    desired_answers = mission["verification"][0]["desiredAnswer"]
 
     print(verif_questions, desired_answers)
 
@@ -42,7 +42,8 @@ def lambda_handler(event, context):
     violation_labels = ["Explicit Nudity", "Sexual Activity"]
 
     violation_detected = any(
-        label["Name"] in violation_labels for label in moderation_labels)
+        label["Name"] in violation_labels for label in moderation_labels
+    )
 
     if violation_detected:
         # Notify the user that the image violates the sensitivity policy
@@ -92,14 +93,14 @@ def lambda_handler(event, context):
     if pass_one_question:
         # Calling API to update mission status
         # Should call Update_Mission here
-        url = "http://192.168.1.111:3000/api/user/updateToday"
+        url = "https://ea9pgpvvaa.execute-api.ap-southeast-1.amazonaws.com/prod/api/user/updateToday"
 
         payload = {
             "userId": userId,
             "missionId": mission["_id"],
         }
 
-    # Define the headers, if any are required. This could include authentication tokens.
+        # Define the headers, if any are required. This could include authentication tokens.
         headers = {
             "Content-Type": "application/json",
         }
@@ -109,8 +110,10 @@ def lambda_handler(event, context):
         if response.status_code == 200:
             print("Successfully updated mission status via API")
         else:
-            print("Failed to update mission status via API. Response code:",
-                  response.status_code)
+            print(
+                "Failed to update mission status via API. Response code:",
+                response.status_code,
+            )
 
     else:
         # Notify user that image isnot verified relation to the challenge.And user should reupload the image
