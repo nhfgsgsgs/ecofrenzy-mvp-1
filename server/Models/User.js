fa6cc760dfab2e660f9d05c016ec58fca8b4fe5e
phamson02 <phamtienson02@gmail.com>
2023-06-18 02:22:55 +0700

    update

--- server/Models/User.js
+++ server/Models/User.js
@@ -2,6 +2,21 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other']
+    },
+    location: {
+      type: String,
+      enum: ['Urban', 'Suburban', 'Rural']
+    },
+    usageDay: {
+      type: Number,
+      default: 0,
+    },
     todayMission: [
       {
         _id: {
@@ -58,10 +73,72 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
-    preferences: {
-      // category: {
-      // }
-    },
+    nextMission: [
+      {
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        point: {
+          type: Number,
+          default: 50,
+        },
+        category: {
+          type: String,
+        },
+        impact: {
+          type: String,
+        },
+        description: {
+          type: String,
+          default: "",
+        },
+        level: {
+          type: String,
+          enum: ["Easy", "Intermediate", "Hard"],
+        },
+        creativity: {
+          type: String,
+          enum: ["Direct", "Indirect"],
+        },
+        verification: [
+          {
+            question: {
+              type: String,
+            },
+            desiredAnswer: {
+              type: String,
+            },
+          },
+        ],
+        status: {
+          type: String,
+          enum: ["Start", "Picked", "Pending", "Done"],
+          default: "Start",
+        },
+        url: {
+          type: String,
+          default: "",
+        },
+        isDone: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    preferences: [
+      {
+        type: String,
+      }
+    ],
+    growth_plan: [
+      {
+        type: String,
+      }
+    ]
   },
   { timestamps: true }
 );
