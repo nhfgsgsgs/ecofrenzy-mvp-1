02a247a2c2fa5407b2357272d111f665a607931c
bachvudinh <bachvudinh02@gmail.com>
2023-06-18 00:29:57 +0700

    update schema

--- server/Models/Storage.js
+++ server/Models/Storage.js
@@ -6,19 +6,59 @@ const storageSchema = new mongoose.Schema({
   },
   historyMission: [
     {
-      name: {
-        type: String,
-      },
-      category: {
-        type: String,
-      },
-      point: {
+      usageDay: {
         type: Number,
       },
-      isDone: {
+      missionPicked: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+      isCompleted: {
         type: Boolean,
         default: false,
       },
+      givenMissions: [
+        {
+          _id: {
+            type: mongoose.Schema.Types.ObjectId,
+          },
+          name: {
+            type: String,
+            required: true,
+          },
+          point: {
+            type: Number,
+            default: 50,
+          },
+          category: {
+            type: String,
+          },
+          impact: {
+            type: String,
+          },
+          description: {
+            type: String,
+            default: "",
+          },
+          level: {
+            type: String,
+            enum: ["Easy", "Intermediate", "Hard"],
+          },
+          creativity: {
+            type: String,
+            enum: ["Direct", "Indirect"],
+          },
+          verification: [
+            {
+              question: {
+                type: String,
+              },
+              desiredAnswer: {
+                type: String,
+              },
+            },
+          ],
+        },
+      ],
     },
   ],
 });
